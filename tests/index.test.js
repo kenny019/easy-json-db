107205f709dee3dfd742cb2a51481e133edebdd2
kenny019 <ngkenny019@gmail.com>
2023-01-24 21:21:44 +0800

    added tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -80,9 +80,117 @@ describe('DBClient', () => {
 				gender: 'female',
 			});
 
-			expect(result.ok).toBeTruthy;
+			const verify = db.get('users', 'test_user');
 
+			expect(result.ok).toBeTruthy;
 			expect(result.val).toBeTruthy;
+			expect(verify.val).toEqual({
+				age: 49,
+				gender: 'female',
+			});
+		});
+
+		test('insert a document containing an object', () => {
+			const db = new DBClient(TEST_DB);
+
+			const result = db.insert('users', 'test_user2', {
+				rules: {
+					admin: true,
+				},
+			});
+
+			const verify = db.get('users', 'test_user2');
+
+			expect(result.ok).toBeTruthy;
+			expect(result.val).toBeTruthy;
+			expect(verify.val).toEqual({
+				rules: {
+					admin: true,
+				},
+			});
+		});
+	});
+
+	describe('Testing update method', () => {
+		test('update a document', () => {
+			const db = new DBClient(TEST_DB);
+
+			const insert = db.insert('users', 'test_user3', {
+				rules: {
+					admin: true,
+				},
+			});
+
+			const update = db.update('users', 'test_user3', {
+				rules: {
+					admin: false,
+				},
+			});
+
+			const get = db.get('users', 'test_user3');
+
+			expect(insert.ok).toBeTruthy;
+			expect(update.ok).toBeTruthy;
+			expect(get.val).toEqual({
+				rules: {
+					admin: false,
+				},
+			});
+		});
+
+		test('update a document not found', () => {
+			const db = new DBClient(TEST_DB);
+
+			const update = db.update('users', 'fake_user', {
+				is_real: true,
+			});
+
+			expect(update.ok).toBeFalsy;
+			expect(update.err).toBeTruthy;
+		});
+	});
+
+	describe('Testing replace method', () => {
+		test('replace a document', () => {
+			const db = new DBClient(TEST_DB);
+
+			const insert = db.insert('users', 'test_user4', {
+				is_guild: false,
+			});
+
+			const replace = db.replace('users', 'test_user4', {});
+
+			expect(insert.ok).toBeTruthy;
+			expect(replace.val).toEqual({});
+		});
+
+		test('replace a document not found', () => {
+			const db = new DBClient(TEST_DB);
+
+			const replace = db.replace('users', 'fake_user2', {});
+
+			expect(replace.ok).toBeFalsy;
+			expect(replace.err).toBeTruthy;
+		});
+	});
+
+	describe('Testing remove method', () => {
+		test('remove a document', () => {
+			const db = new DBClient(TEST_DB);
+
+			const remove = db.remove('users', 'test_user');
+
+			expect(remove.ok).toBeTruthy;
+			expect(remove.err).toBeFalsy;
+		});
+
+		test('remove a non existant document', () => {
+			const db = new DBClient(TEST_DB);
+
+			const remove = db.remove('users', 'fake_user3');
+
+			expect(remove.ok).toBeFalsy;
+			expect(remove.err).toBeTruthy;
 		});
 	});
 });
